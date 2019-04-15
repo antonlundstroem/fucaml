7c125052c1c64d0f02dd0539939a142beb18a359
antonlundstroem <anton.lundstroem@hotmail.se>
2019-04-15 23:17:04 +0200

    added support for ocamlbuild

--- runner.sh
+++ runner.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
-# Compile with first argument as name and second as the soruce file, then execute the binary.
-ocamlc -o $1 $2
-./$1
+
+# Runs all different build options depending on what flags are passed in
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+	key="$1"
+	case $key in
+		-f)
+			BINARY=$2
+			SOURCE=$3
+			ocamlc -o $BINARY $SOURCE
+			break
+			;;
+		-b)
+			BINARY=$2
+			SOURCE=$3
+			ocamlbuild -quiet $BINARY
+			break
+			;;
+		*)
+			BINARY=$1
+			SOURCE=$2
+			ocamlc -o $BINARY $SOURCE
+			break
+			;;
+	esac
+done
+set -- "${POSITIONAL[@]}"
+
+# Runs the newly compiled binary
+./$BINARY
